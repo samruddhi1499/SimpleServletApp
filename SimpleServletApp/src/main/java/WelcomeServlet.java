d4ea8ae8ed5ca99dc6daed16a95a4d72e0ca402c
samruddhi1499 <71025595+samruddhi1499@users.noreply.github.com>
2024-09-05 16:52:31 -0400

    initial commit

---
+++ SimpleServletApp/src/main/java/WelcomeServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet
+public class WelcomeServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Welcome to J2EE Web Components Lab</h1>");
+        out.println("</body></html>");
+    }
+}
